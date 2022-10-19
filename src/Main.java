fdb530eeb098a0e533569891166fe17f3e46efda
dev_delphin <eduard.zakirov.96@mail.ru>
2022-10-19 12:25:17 +0500

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-package apireq;
 
+import apireq.Api;
 import java.io.File;
 import java.io.IOException;
-
 
 public class Main {
 	
 	public static int CONNECTION_TIMEOUT = 15000;
-	public static String contents = "";
-	public static File file = new File("./tmp.txt");
-	public static File jsonfile = new File("./tmp.json");
 	
 	public static void main(String[] args) throws IOException {
 		Api.api(CONNECTION_TIMEOUT);
--- src/apireq/Api.java
+++ src/apireq/Api.java
@@ -12,31 +12,21 @@ public class Api {
 
 		final URL url = new URL("https://api.blockchain.com/v3/exchange/tickers");
         final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        
         con.setRequestMethod("GET");
         con.setRequestProperty("Content-Type", "application/json");
         con.setConnectTimeout(CONNECTION_TIMEOUT);
         con.setReadTimeout(CONNECTION_TIMEOUT );
+        
         try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
             String inputLine;
             final StringBuilder content = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
             }
-            try(FileWriter writer = new FileWriter(Main.file , false))
-            {
-                writer.write(content.toString());
-            }
-            catch(IOException ex){
-                 
-                System.out.println(ex.getMessage());
-            } 
-            
-            Main.contents = content.toString();
             Json.json(content);
-            //return content.toString();
         } catch (final Exception ex) {
             ex.printStackTrace();
-            //return "";
         }
 	}
 }
