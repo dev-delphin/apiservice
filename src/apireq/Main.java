407bf2f9485393654285616c1f66f749034f9e55
v.guselnikov <v.guselniov@greatbook.ru>
2022-10-22 16:00:16 +0300

    Create architecture project. Add Docker compose

--- src/apireq/Main.java
+++ src/apireq/Main.java
@@ -4,17 +4,22 @@ import apireq.server.SrvWork;
 
 public class Main {
 	
-	public static final int CONNECTION_TIMEOUT = 15000;
-	public static final String URL = "https://api.blockchain.com/v3/exchange/tickers";
-	public static final int WAITING_TIME = 30;
-	public static final String HOSTNAME = "localhost";
-	public static final int PORT = 8081;
-	public static final int BACKLOG = 0;
-	public static final int THREAD_POOL = 10;
-	
+	// TODO: move change config to ENV or config file.
+	public static final int CONNECTION_TIMEOUT; // = 15000;
+	public static final String URL; // = "https://api.blockchain.com/v3/exchange/tickers";
+	public static final int WAITING_TIME; // = 30;
+	public static final String HOSTNAME; // = "localhost";
+	public static final int PORT; // = 8081;
+	public static final int BACKLOG; // = 0; 
+	public static final int THREAD_POOL; // = 10;
 
-	public static void main(String[] args) throws IOException {
-		SrvWork.http();
+	Map<String, String> env = System.getenv();
+	for (String envName : env.keySet()) {
+		System.out.format("%s=%s%n",envName,env.get(envName));
 	}
+	
+	// public static void main(String[] args) throws IOException {
+	// 	SrvWork.http();
+	// }
 
 }
