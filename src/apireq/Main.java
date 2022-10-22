903f1632fbd5315f7c5864ecc6f626dbaca8f698
v.guselnikov <v.guselniov@greatbook.ru>
2022-10-22 16:31:38 +0300

    Change moment in Main

--- src/apireq/Main.java
+++ src/apireq/Main.java
@@ -1,25 +1,26 @@
 package apireq;
 import java.io.IOException;
-import apireq.server.SrvWork;
+import java.util.Map;
+// import apireq.server.SrvWork;
 
 public class Main {
 	
 	// TODO: move change config to ENV or config file.
-	public static final int CONNECTION_TIMEOUT; // = 15000;
-	public static final String URL; // = "https://api.blockchain.com/v3/exchange/tickers";
-	public static final int WAITING_TIME; // = 30;
-	public static final String HOSTNAME; // = "localhost";
-	public static final int PORT; // = 8081;
-	public static final int BACKLOG; // = 0; 
-	public static final int THREAD_POOL; // = 10;
+	public static final int CONNECTION_TIMEOUT = 15000;
+	public static final String URL = "https://api.blockchain.com/v3/exchange/tickers";
+	public static final int WAITING_TIME = 30;
+	public static final String HOSTNAME = "localhost";
+	public static final int PORT = 8081;
+	public static final int BACKLOG = 0; 
+	public static final int THREAD_POOL = 10;
 
-	Map<String, String> env = System.getenv();
-	for (String envName : env.keySet()) {
-		System.out.format("%s=%s%n",envName,env.get(envName));
-	}
 	
-	// public static void main(String[] args) throws IOException {
-	// 	SrvWork.http();
-	// }
+	public static void main(String[] args) throws IOException {
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n",envName,env.get(envName));
+		}
+		// SrvWork.http();
+	}
 
 }
